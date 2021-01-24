3ce3ae8f73625ae7d63efb46227dff4a78c5a093
Simon Germscheid <sgermscheid@gmail.com>
2021-01-24 12:40:35 -0600

    setup redux and sagas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+// Redux // Sagas
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from './redux/reducers/_root.reducer';
+import rootSaga from './redux/sagas/_root.saga';
+// redux longer
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+
+// Component Imports
+import App from './components/App/App';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewareList = process.env.NODE_ENV === 'development' ?
+  [sagaMiddleware, logger] :
+  [sagaMiddleware];
+
+// Create Store
+const store = createStore(
+  rootReducer,
+  applyMiddleware(...middlewareList),
+);
+
+sagaMiddleware.run(rootSaga);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider >
   </React.StrictMode>,
   document.getElementById('root')
 );
